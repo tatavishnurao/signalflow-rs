f9157b95c6f0e376f700f3561062c3d6677ef146
tatavishnurao <tatavishnurao@gmail.com>
2026-06-29 23:34:52 +0530

    main entry point

---
+++ src/main.rs
@@ -0,0 +1,13 @@
+use realtime_audio_accelerator::{config::AppConfig, pipeline::run_dummy_pipeline};
+
+fn main() -> anyhow::Result<()> {
+    let config = AppConfig::default();
+    let report = run_dummy_pipeline(&config)?;
+
+    println!(
+        "dummy pipeline: samples={}, frame={} hop={} rms={:.6}",
+        report.num_samples, report.frame_size_samples, report.hop_size_samples, report.rms_energy
+    );
+
+    Ok(())
+}
