6ccc112214e4992270c2534b01d3fd459d46c76d
tatavishnurao <tatavishnurao@gmail.com>
2026-06-30 23:04:08 +0530

    Add config tests

--- src/config.rs
+++ src/config.rs
@@ -26,3 +26,28 @@ impl AppConfig {
         (self.sample_rate_hz as usize * self.hop_ms as usize) / 1_000
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::AppConfig;
+
+    #[test]
+    fn default_config_has_expected_values() {
+        let config = AppConfig::default();
+
+        assert_eq!(config.sample_rate_hz, 16_000);
+        assert_eq!(config.channels, 1);
+        assert_eq!(config.frame_ms, 25);
+        assert_eq!(config.hop_ms, 10);
+    }
+
+    #[test]
+    fn frame_size_samples_is_400() {
+        assert_eq!(AppConfig::default().frame_size_samples(), 400);
+    }
+
+    #[test]
+    fn hop_size_samples_is_160() {
+        assert_eq!(AppConfig::default().hop_size_samples(), 160);
+    }
+}
