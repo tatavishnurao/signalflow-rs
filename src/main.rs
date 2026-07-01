240a071a3acbf48e6e41693883d4d26394d406f8
tatavishnurao <tatavishnurao@gmail.com>
2026-07-01 23:52:45 +0530

    Update pipeline output

--- src/main.rs
+++ src/main.rs
@@ -5,8 +5,12 @@ fn main() -> anyhow::Result<()> {
     let report = run_dummy_pipeline(&config)?;
 
     println!(
-        "dummy pipeline: samples={}, frame={} hop={} rms={:.6}",
-        report.num_samples, report.frame_size_samples, report.hop_size_samples, report.rms_energy
+        "dummy pipeline: samples={}, frame={} hop={} frames={} rms={:.6}",
+        report.num_samples,
+        report.frame_size_samples,
+        report.hop_size_samples,
+        report.num_frames,
+        report.rms_energy
     );
 
     Ok(())
